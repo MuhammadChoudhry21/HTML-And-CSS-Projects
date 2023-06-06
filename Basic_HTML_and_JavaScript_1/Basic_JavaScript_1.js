cb18a78f980d4afd17ab675fed0665be51b4681a
MuhammadChoudhry21 <135356860+MuhammadChoudhry21@users.noreply.github.com>
2023-06-06 11:01:15 -0400

    Basic_HTML_and_JavaScript_1 assignment
    all the files in Basic_HTML_and_JavaScript_1 folder

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+alert("This is an alert from an external javascript file!");
+function My_First_Function() {
+		var String = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementByld("Irish").innterHTML =
+		result;
+	}
